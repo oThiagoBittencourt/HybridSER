e335c2fdf24a7c0ce1435412df04424e18715116
Thesca <thiaguescaliante@gmail.com>
2025-07-15 21:23:59 -0300

    saving changes

--- utils/extract_lib.py
+++ utils/extract_lib.py
@@ -19,33 +19,32 @@ delta2_mfccs = librosa.feature.delta(data = mfccs, order = 2)
 #print(delta2_mfccs.shape)
 
 # plt.figure(figsize=(25, 10))
-# librosa.display.specshow(delta_mfccs, sr=sr, x_axis='time')
+# librosa.display.specshow(delta2_mfccs, sr=sr, x_axis='time')
 # plt.colorbar()
 # plt.title('MFCC')
 # plt.tight_layout()
-# #plt.show()
-
-
-rms_values = librosa.feature.rms(y=signal)[0]
-#times = librosa.frames_to_time(np.arange(len(rms_values)), sr=sr)
-
-# plt.figure(figsize=(25, 10))
-# plt.plot(times, rms_values) # Eixo X é o tempo, Eixo Y são os valores RMS
-# plt.title('RMS Energy Over Time')
-# plt.xlabel('Time (s)')
-# plt.ylabel('RMS Energy')
-# plt.grid(True) # Adiciona uma grade para melhor leitura
-# plt.tight_layout()
 # plt.show()
 
-chromagram = librosa.feature.chroma_stft(y=signal, sr=sr, n_fft=4096) 
+chromagram = librosa.feature.chroma_stft(y=signal, sr=sr, n_fft=4096)
 
-# plt.figure(figsize=(25, 10))
+plt.figure(figsize=(25, 10))
 
 # librosa.display.specshow(chromagram, sr=sr, x_axis='time', y_axis='chroma')
 
-# plt.colorbar(label='Intensidade')
+# plt.colorbar(label='Intensity')
 # plt.title('Chromagram')
+# plt.tight_layout()
+# plt.show()
+
+rms_values = librosa.feature.rms(y=signal)[0]
+times = librosa.frames_to_time(np.arange(len(rms_values)), sr=sr)
+
+# plt.figure(figsize=(25, 10))
+# plt.plot(times, rms_values)
+# plt.title('RMS Energy Over Time')
+# plt.xlabel('Time (s)')
+# plt.ylabel('RMS Energy')
+# plt.grid(True)
 # plt.tight_layout()
 # plt.show()
 
@@ -53,11 +52,16 @@ zcr = librosa.feature.zero_crossing_rate(y=signal)[0]
 
 times = librosa.frames_to_time(np.arange(len(zcr)), sr=sr)
 
-plt.figure(figsize=(25, 10))
-plt.plot(times, zcr)
-plt.title('Zero-Crossing Rate Over Time')
-plt.xlabel('Time (s)')
-plt.ylabel('ZCR')
-plt.grid(True)
-plt.tight_layout()
-plt.show()
+# plt.figure(figsize=(25, 10))
+# plt.plot(times, zcr)
+# plt.title('Zero-Crossing Rate Over Time')
+# plt.xlabel('Time (s)')
+# plt.ylabel('ZCR')
+# plt.grid(True)
+# plt.tight_layout()
+# plt.show()
+
+#####  TODO think about how should be the pipeline to transform the datasets 
+# into an feedable dataset with all the extractions
+
+#####  See encoder in latency array
