4d367553a2af330f8758f2eafbf71dd2913fe690
Thesca <thiaguescaliante@gmail.com>
2025-07-22 22:37:31 -0300

    chore: Adapt requirements for pip installation in VM at github workflow

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,7 +1,6 @@
 import json
 import os
 
-# --- Função para Carregar a Configuração ---
 def load_config():
     """
     Finds and loads config.json from project root.
@@ -10,14 +9,6 @@ def load_config():
     project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     config_path = os.path.join(project_root, 'config.json') 
 
-    # Abre e lê o arquivo JSON
-    try:
-        with open(config_path, 'r', encoding='utf-8') as f:
-            config = json.load(f)
-        return config
-    except FileNotFoundError:
-        print(f"Erro: O arquivo de configuração não foi encontrado em '{config_path}'")
-        return None
-    except json.JSONDecodeError:
-        print(f"Erro: O arquivo '{config_path}' não é um JSON válido.")
-        return None
+    with open(config_path, 'r', encoding='utf-8') as f:
+        config = json.load(f)
+    return config
