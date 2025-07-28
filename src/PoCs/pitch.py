869536feea7afdb3f760687348d9e991474e3153
Yann Lucas <101208372+Y4nnLS@users.noreply.github.com>
2025-07-28 20:19:08 -0300

    feat: add POC that aplly pitch shifting on an audio file

---
+++ src/PoCs/pitch.py
@@ -0,0 +1,15 @@
+import soundfile as sf
+import librosa
+from utils.augment import pitch_shift
+import os
+
+OUTPUT_DIR = "audios/outputs"
+os.makedirs(OUTPUT_DIR, exist_ok=True) 
+
+y, sr = librosa.load("audios/03-01-02-01-01-02-01.wav", sr=None)
+print(f"Original: {y.shape}, SR = {sr}")
+
+y_pitch = pitch_shift(y, sr, steps=2)
+
+sf.write(f"{OUTPUT_DIR}/pitch.wav", y_pitch, sr)
+print("Arquivo com pitch shift salvo.")
