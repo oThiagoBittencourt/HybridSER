ca3e1690dee6c332cd2a7350c7242191e8aebf9e
Y4nnLS <yann_lucas04@hotmail.com>
2025-08-06 17:48:37 -0300

    test: add unit test for specAugment

--- tests/test_augment.py
+++ tests/test_augment.py
@@ -1,9 +1,13 @@
 import numpy as np
-from src.utils.augment import add_awgn, pitch_shift, time_stretch
+from src.utils.augment import add_awgn, pitch_shift, time_stretch, spec_augment
 
 
 def generate_dummy_audio(duration_sec=1, sr=16000):
     return np.ones(sr * duration_sec), sr
+
+
+def generate_dummy_spectrogram(n_mels=128, n_frames=100):
+    return np.ones((n_mels, n_frames), dtype=np.float32)
 
 
 def test_add_awgn():
@@ -23,3 +27,11 @@ def test_time_stretch():
     y, _ = generate_dummy_audio()
     y_stretched = time_stretch(y, rate=0.9)
     assert y_stretched.shape[0] != y.shape[0]
+
+
+def test_spec_augment():
+    spec = generate_dummy_spectrogram()
+    augmented_spec = spec_augment(spec)
+
+    assert augmented_spec.shape == spec.shape, "Dimensão do espectrograma deve permanecer igual"
+    assert not np.array_equal(spec, augmented_spec), "A saída do specAugment não deve ser idêntica à entrada"
